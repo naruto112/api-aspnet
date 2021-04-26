ff58a112b87fbabda9b7dab0a19a48ff2628d9b6
Renato Souza <renatorock3@hotmail.com>
2021-04-26 09:51:20 -0300

    commit

---
+++ WebApplication/App_Start/WeApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace WebApplication.App_Start
+{
+    public class WeApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            var routes = config.Routes;
+
+            
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            config.EnableCors();
+
+            config.MapHttpAttributeRoutes();
+            
+            routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+        }
+    }
+}
---
+++ WebApplication/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Users
+    {
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+    }
+}
